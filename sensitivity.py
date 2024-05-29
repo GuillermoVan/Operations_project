2365e70fd93669f9235575fdeecb13431fc73c2f
jelskamp <jelskamp@student.tudelft.nl>
2024-05-29 10:55:49 +0200

    update sens

--- sensitivity.py
+++ sensitivity.py
@@ -30,14 +30,16 @@ class Sensitivity:
         parameter_settings_sensitivity = {
             'p': self.parameter_settings['p'] * factor,
             'C': self.parameter_settings['C'],
-            'I0': self.parameter_settings['I0'],
-            's0': self.parameter_settings['s0'] * factor,
-            'h0': self.parameter_settings['h0'] * factor
+            's': self.parameter_settings['s'] * factor,
+            'h0': self.parameter_settings['h0'] * factor,
+            'l': self.parameter_settings['l'] * factor
         }
         return parameter_settings_sensitivity
 
 # Example usage (change to current param settings):
-parameter_settings = {'p': 1.5/60, 'C': 15, 'I0': 30, 's0': 100, 'h0': 5}
+# parameter_settings = {'p': 1.5/60, 'C': 15, 'I0': 30, 's': 100, 'h0': 5}
+
+parameter_settings = {'p': 1, 'C': 500, 's': 100, 'h0': 0.1, 'l': 1}
 
 if __name__ == "__main__":
     sensitivity_analysis = Sensitivity(model_name="static_ACP", T=24, l=1/12, parameter_settings=parameter_settings)
